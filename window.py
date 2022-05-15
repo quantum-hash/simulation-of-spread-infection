2e6507beeb518e919976bab8e984fe046f642a18
AB <96754248+quantum-hash@users.noreply.github.com>
2022-05-15 14:33:23 +0200

    Update window.py

--- window.py
+++ window.py
@@ -1,7 +1,7 @@
 import pygame
 
 class fenetre:
-    def __init__(self, longueur, largeur, nom_fenetre="Simulation epidemie GEII", nom_icone='icone.jpg'):
+    def __init__(self, longueur: int, largeur: int, nom_fenetre: str = "Simulation epidemie GEII", nom_icone: str = 'icone.jpg') -> None:
         self.fenetre = pygame.display.set_mode(
             (longueur, largeur))  # Creation de la fenetre
         pygame.display.set_caption(nom_fenetre)
@@ -11,7 +11,10 @@ class fenetre:
         except FileNotFoundError:
             pass
 
-    def creer_environnement(self, taille_env, couleur, forme="carre"):
+    def creer_environnement(self, taille_env: tuple, couleur: tuple, forme: "carre" or "cercle" = "carre") -> None:
+        """
+        couleur: tuple(red: int, green: int, blue: int)
+        """
         if forme == "carre":
             self.environnement = pygame.draw.rect(
                 self.fenetre, couleur, pygame.Rect(30, 30, taille_env[0], taille_env[1]),  1)
